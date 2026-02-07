7daff137c469173f6fae34226250d6c24441e696
vaishnavi491 <vaishnavimorey51@gmail.com>
2026-02-07 13:26:40 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,49 @@
+const toggleBtn = document.getElementById("themeToggle");
+toggleBtn.addEventListener("click", () => {
+  document.body.classList.toggle("light-theme");
+  localStorage.setItem(
+    "theme",
+    document.body.classList.contains("light-theme") ? "light" : "dark",
+  );
+  const isLightTheme = document.body.classList.contains("light-theme");
+  toggleBtn.textContent = isLightTheme ? "🌙" : "🌞";
+});
+
+//array typing custom function.....
+const typedTextSpan = document.querySelector(".typed-text");
+const textArray = ["Web Developer", "Data Analyst", "Software Engineer","Tech Enthusiast"];
+const typingDelay = 100;
+const erasingDelay = 50;
+const newTextDelay = 2000; // Delay between current and next text
+let textArrayIndex = 0;
+let charIndex = 0;
+
+function type() {
+  if (charIndex < textArray[textArrayIndex].length) {
+    typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
+    charIndex++;
+    setTimeout(type, typingDelay);
+  } else {
+    setTimeout(erase, newTextDelay);
+  }
+}
+
+function erase() {
+  if (charIndex > 0) {
+    typedTextSpan.textContent = textArray[textArrayIndex].substring(
+      0,
+      charIndex - 1
+    );
+    charIndex--;
+    setTimeout(erase, erasingDelay);
+  } else {
+    textArrayIndex++;
+    if (textArrayIndex >= textArray.length) textArrayIndex = 0;
+    setTimeout(type, typingDelay + 1100);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  // On DOM Load initiate the effect
+  if (textArray.length) setTimeout(type, newTextDelay + 250);
+});
